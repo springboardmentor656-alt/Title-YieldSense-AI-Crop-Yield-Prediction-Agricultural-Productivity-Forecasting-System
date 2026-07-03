be3461be8899953477a2f202398b6cbf012205e1
Varnit Pandit <varnitpandit01@gmail.com>
2026-07-03 10:41:26 +0530

    Completed authentication module

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine, Base
+from models import User
+from schemas import UserCreate, UserLogin
+from auth import hash_password, verify_password, create_access_token
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home():
     return {"message": "YieldSense AI Running Successfully"}
+
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hash_password(user.password),
+        role=user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
+
+
+@app.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    token = create_access_token(
+        {"sub": db_user.email, "role": db_user.role}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
