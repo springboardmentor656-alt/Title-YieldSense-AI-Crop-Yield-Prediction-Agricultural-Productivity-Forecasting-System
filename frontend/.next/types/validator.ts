5c4a9d97065732106e1d5a53400f8065a9adac04
charanreddysuggu <charanreddysuggu84@gmail.com>
2026-07-16 20:10:25 +0530

    Added <worked on Milestone-2>

--- frontend/.next/types/validator.ts
+++ frontend/.next/types/validator.ts
@@ -72,6 +72,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../app/predict/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/predict">> = Specific
+  const handler = {} as typeof import("../../app/predict/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
